7e302dbcfa998e0f6153f5395e701d2ac07fff38
Joshua <JoshuaJB@users.noreply.github.com>
2015-06-03 14:42:03 -0400

    Add

--- src/main.c
+++ src/main.c
@@ -3,32 +3,43 @@
 Window *my_window;
 TextLayer *text_layer;
 
-// Global number of accelerometer samples taken
-int sample_num = 0;
-
-// Called every 25 samples with accelerometer data
-static void display_acceleration(AccelData * data, uint32_t num_samples) {
-  sample_num += num_samples;
-  // Compute local average
-  int x_avg = 0;
-  int y_avg = 0;
-  int z_avg = 0;
-  for (unsigned int i = 0; i < num_samples; i++) {
-    x_avg += data[i].x;
-    y_avg += data[i].y;
-    z_avg += data[i].z;
+float xsqrt(const float num) {
+  const unsigned int MAX_STEPS = 40;
+  const float MAX_ERROR = 0.001;
+  
+  float answer = num;
+  float ans_sqr = answer * answer;
+  unsigned int step = 0;
+  while ((ans_sqr - num > MAX_ERROR) && (step++ < MAX_STEPS)) {
+    answer = (answer + (num / answer)) / 2;
+    ans_sqr = answer * answer;
   }
-  APP_LOG(APP_LOG_LEVEL_INFO, "x_avg: %d, num_samples: %d", x_avg, (int)num_samples);
-  x_avg /= (int)num_samples;
-  y_avg /= 25;
-  z_avg /= 25;
+  return answer;
+}
+
+// Called with accelerometer data samples
+static void display_acceleration(AccelData * data, uint32_t num_samples) {
+  // Compute local average
+  int local_x_avg = 0;
+  int local_y_avg = 0;
+  int local_z_avg = 0;
+  for (unsigned int i = 0; i < num_samples; i++) {
+    local_x_avg += data[i].x;
+    local_y_avg += data[i].y;
+    local_z_avg += data[i].z;
+  }
+  local_x_avg /= (int)num_samples;
+  local_y_avg /= 25;
+  local_z_avg /= 25;
+  int variance = xsqrt(local_x_avg*local_x_avg + local_y_avg*local_y_avg + local_z_avg*local_z_avg) - 1000;
   // Display local average
-  static char display_string[57]; // 10 places for each avg
-  snprintf(display_string, 57, "Local Average:\nX:%d,\nY:%d,\nZ:%d\nS:%u", (int)x_avg, (int)y_avg, (int)z_avg, (unsigned int)num_samples);
+  static char display_string[85];
+  snprintf(display_string, 85, "%d Sample Average:\nX:%d,\nY:%d,\nZ:%d\nVariance From Zero\n%d", (int)num_samples, local_x_avg, local_y_avg, local_z_avg, variance);
   text_layer_set_text(text_layer, display_string);
 }
 
 static void main_window_load(Window *window) {
+  // Setup the text layer
   text_layer = text_layer_create(layer_get_bounds(window_get_root_layer(my_window)));
   text_layer_set_text(text_layer, "Acquiring data...");
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_layer));
@@ -40,6 +51,7 @@ static void main_window_load(Window *window) {
 
 void handle_init(void) {
   my_window = window_create();
+  // Setup main_window_load to run once the window loads
   window_set_window_handlers(my_window, (WindowHandlers){.load = main_window_load});
   window_stack_push(my_window, true);
 }
